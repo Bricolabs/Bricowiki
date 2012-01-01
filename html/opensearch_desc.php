ea546b21df0e4363d5f17df7d2634b03e40b39f8
Ian Lawrence <root@ianlawrence.info>
2012-01-01 11:27:10 -0400

    Added static html and markdown

---
+++ html/opensearch_desc.php
@@ -0,0 +1,7 @@
+<?xml version="1.0"?>
+<OpenSearchDescription xmlns="http://a9.com/-/spec/opensearch/1.1/">
+<ShortName>Bricowiki (English)</ShortName>
+<Description>Bricowiki (English)</Description>
+<Image height="16" width="16" type="image/x-icon">http://wiki.bricolabs.net/favicon.ico</Image>
+<Url type="text/html" method="get" template="http://wiki.bricolabs.net/index.php?title=Special:Search&amp;search={searchTerms}"/>
+</OpenSearchDescription>
